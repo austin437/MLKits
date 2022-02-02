e68de034c6033c533ecd03e9fdd201ef96f6430d
Robert Austin <austin437@hotmail.com>
2022-02-02 08:45:02 +0000

    ran prettier

--- plinko/score.js
+++ plinko/score.js
@@ -1,37 +1,34 @@
 const outputs = [];
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-    outputs.push([dropPosition, bounciness, size, bucketLabel])
+    outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
     const testSetSize = 50;
     const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
 
-    _.range(1, 15).forEach(k => {
+    _.range(1, 15).forEach((k) => {
         const accuracy = _.chain(testSet)
-            .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3])
+            .filter((testPoint) => knn(trainingSet, _.initial(testPoint), k) === testPoint[3])
             .size()
             .divide(testSetSize)
             .value();
 
-        console.log('For k of', k, 'accuracy is:', accuracy);
-    })
+        console.log("For k of", k, "accuracy is:", accuracy);
+    });
 }
 
 function knn(data, point, k) {
     return _.chain(data)
-        .map(row => {
-            return [
-                distance(_.initial(row), point),
-                _.last(row)
-            ]
+        .map((row) => {
+            return [distance(_.initial(row), point), _.last(row)];
         })
-        .sortBy(row => row[0])
+        .sortBy((row) => row[0])
         .slice(0, k)
-        .countBy(row => row[1])
+        .countBy((row) => row[1])
         .toPairs()
-        .sortBy(row => row[1])
+        .sortBy((row) => row[1])
         .last()
         .first()
         .parseInt()
@@ -39,19 +36,20 @@ function knn(data, point, k) {
 }
 
 function distance(pointA, pointB) {
-    return _.chain(pointA)
-        .zip(pointB)
-        .map(([a, b]) => (a - b) ** 2)
-        .sum()
-        .value() ** 0.5;
+    return (
+        _.chain(pointA)
+            .zip(pointB)
+            .map(([a, b]) => (a - b) ** 2)
+            .sum()
+            .value() ** 0.5
+    );
 }
 
 function splitDataset(data, testCount) {
-    const shuffled = _.shuffle(data)
+    const shuffled = _.shuffle(data);
 
-    const testSet = _.slice(shuffled, 0, testCount)
-    const trainingSet = _.slice(shuffled, testCount)
+    const testSet = _.slice(shuffled, 0, testCount);
+    const trainingSet = _.slice(shuffled, testCount);
 
     return [testSet, trainingSet];
 }
-
