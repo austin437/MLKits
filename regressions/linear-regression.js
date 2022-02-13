438c332a69c368329e67d3f3ef8a5972788e84ec
Robert Austin <austin437@hotmail.com>
2022-02-13 14:36:24 +0000

    linear-regression class upated@

--- regressions/linear-regression.js
+++ regressions/linear-regression.js
@@ -4,6 +4,8 @@ class LinearRegression {
     constructor(features, labels, options) {
         this.features = features;
         this.labels = labels;
+
+        //modify to use this.options = {learningRate: 0.1, someDefaultVal: 3.4, ...options};
         this.options = Object.assign({ learningRate: 0.1 }, options);
     }
 }
