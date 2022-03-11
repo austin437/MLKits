7ddbc44901694865ebc39da5fb698dc05b77c540
Robert Austin <austin437@hotmail.com>
2022-03-11 08:12:36 +0000

    linear-regression.js: updated to use batch gradient descent

--- regressions/linear-regression.js
+++ regressions/linear-regression.js
@@ -8,21 +8,32 @@ module.exports = class LinearRegression {
         this.mseHistory = [];
 
         //modify to use this.options = {learningRate: 0.1, someDefaultVal: 3.4, ...options};
-        this.options = Object.assign({ learningRate: 0.1, iterations: 1000 }, options);
+        this.options = Object.assign({ learningRate: 0.1, iterations: 1000, batchSize: 10 }, options);
 
         this.weights = tf.zeros([this.features.shape[1], 1]);
     }
 
-    gradientDescent() {
-        const currentGuesses = this.features.matMul(this.weights);
-        const differences = currentGuesses.sub(this.labels);
-        const slopes = this.features.transpose().matMul(differences).div(this.features.shape[0]);
+    gradientDescent(features, labels) {
+        const currentGuesses = features.matMul(this.weights);
+        const differences = currentGuesses.sub(labels);
+        const slopes = features.transpose().matMul(differences).div(features.shape[0]);
         this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
     }
 
     train() {
+        const batchQuantity = Math.floor(this.features.shape[0] / this.options.batchSize);
+
         for (let i = 0; i < this.options.iterations; i++) {
-            this.gradientDescent();
+            for (let j = 0; j < batchQuantity; j++) {
+                const { batchSize } = this.options;
+                const startIndex = j * batchSize;
+
+                const featureSlice = this.features.slice([startIndex, 0], [batchSize, -1]);
+                const labelSlice = this.labels.slice([startIndex, 0], [batchSize, -1]);
+
+                this.gradientDescent(featureSlice, labelSlice);
+            }
+
             this.recordMSE();
             this.updateLearningRate();
         }
